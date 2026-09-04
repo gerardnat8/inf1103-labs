980c14a03cee6571f2618b62efb63decd4fb8461
Isaiah <2601447@sit.singaporetech.edu.sg>
2026-09-04 16:51:52 +0800

    information

--- hello.py
+++ hello.py
@@ -2,3 +2,16 @@ print("=============================")
 print('Welcome here')
 print('My first post!')
 print('=============================')
+
+username =  "cool creator"
+bio = "Fun Blogger"
+followers = "100"
+
+print('Username:', username)
+print("Bio:", bio)
+print("Followers:", followers)
+
+username = "lame creator"
+
+print('Username:', username)
+
