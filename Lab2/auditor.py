a487fa958f43f412715a892c65b5f4936399e776
Isaiah <2601447@sit.singaporetech.edu.sg>
2026-09-11 17:34:04 +0800

    quit

--- Lab2/auditor.py
+++ Lab2/auditor.py
@@ -8,3 +8,14 @@ failed_inventories = 0
 
 print("Smart Inventory Auditor Initialised")
 print("Enter stock quantities to add. Type 'quit' to exit.\n")
+
+while True:
+    user_input = input("Enter stock quantity: ").strip()
+
+    if user_input.lower() == "quit":
+        print("/nLeaving system due to user command.")
+        break
+
+    if not user_input.isdigit():
+        if user_input.startswith('-') and user_input[1:].isdigit():
+
