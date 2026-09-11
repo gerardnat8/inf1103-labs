ddb2a7fef97db7a1601d0c9df3edca226734b660
Isaiah <2601447@sit.singaporetech.edu.sg>
2026-09-11 17:11:58 +0800

    inventory initialised

--- Lab2/auditor.py
+++ Lab2/auditor.py
@@ -2,3 +2,9 @@ print("=============================")
 print('Smart Inventory Auditor')
 print("=============================")
 
+total_inventories = 0
+
+failed_inventories = 0 
+
+print("Smart Inventory Auditor Initialised")
+print("Enter stock quantities to add. Type 'quit' to exit.\n")
