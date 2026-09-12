f34955a1a0199158de4030ba826c34f7eced1b4a
Isaiah <2601447@sit.singaporetech.edu.sg>
2026-09-12 11:25:05 +0800

    Final Report

--- Lab2/auditor.py
+++ Lab2/auditor.py
@@ -18,15 +18,15 @@ while True:
 
     if not user_input.isdigit():
             if user_input.startswith('-') and user_input[1:].isdigit():
-                 print("Input Error: Negative numbers are not allowed.")
+                 print("Input Error: Negative numbers are not allowed.\n")
             else:
-                 print(f"Input Error: {user_input} is not a valid integer.")
+                 print(f"Input Error: {user_input} is not a valid integer.\n")
             failed_entries += 1
             continue 
 
     quantity = int(user_input)
     total_inventories += quantity
-    print(f"Success: Added {quantity} to total inventory. Current total: {total_inventories}")
+    print(f"Success: Added {quantity} to total inventory. Current total: {total_inventories}\n")
 
     if total_inventories > 500:
          print(f"\nTotal units {total_inventories} has exceeded the maximum of 500")
@@ -38,9 +38,12 @@ while True:
          pass
         
 
-
-        
-
+print("\n" + "="*38)
+print("FINAL AUDIT METRICS REPORT")
+print("="*38)
+print(f"Total Units Processed: {total_inventories} units")
+print(f"Number of Failed Entries: {failed_entries} entries")
+print("="*38 + "\n")
 
     
 
